fbfb32ca4da384b568c42381b1003c0c05b1ace3
baltop <baltop@gmail.com>
2021-11-26 10:35:40 +0900

    watchbox

--- watchBox.go
+++ watchBox.go
@@ -4,30 +4,33 @@ import (
 	"avro2pg/database"
 	"encoding/json"
 	"fmt"
-	"strings"
 )
 
-type WatchBox struct {
-	ChargerNo string `json:"charge_no"`
-	CheckDate string `json:"check_date"`
-	Status    []int  `json:"status"`
-	Company   string `json:"company"`
-	RoomName  string `json:"room_name"`
-}
+// type WatchBox struct {
+// 	ChargerNo string `json:"charge_no"`
+// 	CheckDate string `json:"check_date"`
+// 	Status    []int  `json:"status"`
+// 	Company   string `json:"company"`
+// 	RoomName  string `json:"room_name"`
+// }
 
 func saveWatchBoxStatus(originStr string) {
 	// loc, _ := time.LoadLocation("Asia/Seoul")
 
-	watchBox := WatchBox{}
+	defer func() {
+		msg := recover()
+		fmt.Println(msg)
+	}()
+
+	watchBox := database.WatchBox{}
 	json.Unmarshal([]byte(originStr), &watchBox)
 
-	watchBoxDb := database.WatchBox{}
-	watchBoxDb.ChargerNo = watchBox.ChargerNo
-	watchBoxDb.CheckDate = watchBox.CheckDate
-	watchBoxDb.Status = strings.Trim(strings.Replace(fmt.Sprint(watchBox.Status), " ", ",", -1), "[]")
-	watchBoxDb.Company = watchBox.Company
-	watchBoxDb.RoomName = watchBox.RoomName
+	// watchBoxDb := database.WatchBox{}
+	// watchBoxDb.ChargerNo = watchBox.ChargerNo
+	// watchBoxDb.CheckDate = watchBox.CheckDate
+	// watchBoxDb.Status = strings.Trim(strings.Replace(fmt.Sprint(watchBox.Status), " ", ",", -1), "[]")
+	// watchBoxDb.Company = watchBox.Company
+	// watchBoxDb.RoomName = watchBox.RoomName
 
-
-	database.DB.Save(&watchBoxDb)
+	database.DB.Save(&watchBox)
 }
